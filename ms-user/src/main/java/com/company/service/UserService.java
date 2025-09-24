37ab530455d13cbf15304920b490150e1ff6ca93
nijat <khanalievn@gmail.com>
2025-09-24 13:03:49 +0400

    structure create

---
+++ ms-user/src/main/java/com/company/service/UserService.java
@@ -0,0 +1,14 @@
+package com.company.service;
+
+import com.company.model.dto.request.RegistrationRequest;
+import com.company.model.dto.response.UserResponse;
+import com.company.model.dto.response.UserResponseWithPassword;
+
+public interface UserService {
+
+    void register(RegistrationRequest request);
+
+    UserResponse getUserById(Long userId);
+
+    UserResponseWithPassword getUserByEmail(String email);
+}

cc9329c7b1162dfacac95992e911f1f9edaae425
nijat <khanalievn@gmail.com>
2025-09-24 15:57:54 +0400

    UserController created

--- ms-user/src/main/java/com/company/controller/UserController.java
+++ ms-user/src/main/java/com/company/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("v1/users")
 @RequiredArgsConstructor
 public class UserController {
 

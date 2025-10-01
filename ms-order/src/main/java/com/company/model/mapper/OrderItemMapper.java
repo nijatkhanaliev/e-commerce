f4766593df6987952caba2281fcd7b6a868625ca
nijat <khanalievn@gmail.com>
2025-10-01 23:15:49 +0400

    OrderITEMMAPPER CLASS created

---
+++ ms-order/src/main/java/com/company/model/mapper/OrderItemMapper.java
@@ -0,0 +1,32 @@
+package com.company.model.mapper;
+
+import com.company.dao.entity.OrderItem;
+import com.company.model.dto.request.OrderItemRequest;
+import com.company.model.dto.response.OrderItemResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE)
+public interface OrderItemMapper {
+
+    @Mapping(target = "totalPrice", source = "request", qualifiedByName = "getTotalPrice")
+    OrderItem toOrderItem(OrderItemRequest request);
+
+    List<OrderItem> toOrderItems(List<OrderItemRequest> requests);
+
+    OrderItemResponse toOrderItemResponse(OrderItem orderItem);
+
+    @Named(value = "getTotalPrice")
+    default BigDecimal getTotalPrice(OrderItemRequest request) {
+        return request.getPrice()
+                .multiply(BigDecimal.valueOf(request.getQuantity()));
+    }
+
+
+}

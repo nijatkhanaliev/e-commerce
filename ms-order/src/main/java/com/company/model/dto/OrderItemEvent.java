dc98ba59ed2c02f18d51304d3c5fff764a38d289
nijat <khanalievn@gmail.com>
2025-10-01 23:15:15 +0400

    OrderItemEvent created

---
+++ ms-order/src/main/java/com/company/model/dto/OrderItemEvent.java
@@ -0,0 +1,19 @@
+package com.company.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class OrderItemEvent implements Serializable {
+    private Long productId;
+    private int quantity;
+
+    public OrderItemEvent(Long productId, int quantity){
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+}

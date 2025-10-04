9834670c02276eaf39ca1c483f67b58b757c969e
nijatkhanaliev <khanalievn@gmail.com>
2025-10-04 15:25:32 +0400

    getOrderByIdAnUserId query created

--- ms-order/src/main/java/com/company/dao/repository/OrderRepository.java
+++ ms-order/src/main/java/com/company/dao/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.company.dao.repository;
 import com.company.dao.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
 }

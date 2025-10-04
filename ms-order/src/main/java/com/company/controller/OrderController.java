5ee4fc4765ccaf998aab93c3a84fa485d472c819
nijatkhanaliev <khanalievn@gmail.com>
2025-10-04 15:25:08 +0400

    Get order details api implemented

--- ms-order/src/main/java/com/company/controller/OrderController.java
+++ ms-order/src/main/java/com/company/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.company.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -28,6 +30,14 @@ public class OrderController {
 
         return ResponseEntity.status(CREATED)
                 .body(orderService.createOrder(request, userId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> getOrderDetails(
+            @RequestHeader("current-user-id") Long userId,
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(orderService.getOrderDetails(id, userId));
     }
 
 }

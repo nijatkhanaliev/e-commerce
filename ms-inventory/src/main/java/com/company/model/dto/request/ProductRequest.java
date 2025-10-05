a6afa96d37353c5862dcb4e9a8df2c5a6d2cda3b
nijatkhanaliev <nicatxanaliev@gmail.com>
2025-10-05 22:58:57 +0400

    ProducRequest class changed

--- ms-inventory/src/main/java/com/company/model/dto/request/ProductRequest.java
+++ ms-inventory/src/main/java/com/company/model/dto/request/ProductRequest.java
@@ -3,7 +3,6 @@ package com.company.model.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,6 +22,6 @@ public class ProductRequest {
     private BigDecimal price;
 
     @NotNull
-    @PositiveOrZero
+    @Positive
     private Integer stock;
 }

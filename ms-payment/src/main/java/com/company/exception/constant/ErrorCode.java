8a54dd208e5c0148959c377484f1fe451041eab8
nijatkhanaliev <nicatxanaliev@gmail.com>
2025-10-10 01:52:43 +0400

    payment ms created

---
+++ ms-payment/src/main/java/com/company/exception/constant/ErrorCode.java
@@ -0,0 +1,6 @@
+package com.company.exception.constant;
+
+public interface ErrorCode {
+    String DATA_NOT_FOUND = "data_not_found";
+    String EMPTY_ORDER_ITEMS = "empty_order_items";
+}
---
+++ ms-payment/src/main/java/com/company/model/mapper/PaymentMapper.java
@@ -0,0 +1,2 @@
+package com.company.model.mapper;public interface PaymentMapper {
+}

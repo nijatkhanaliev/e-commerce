f98be97f937c67318859470cd05dd5efff207d25
nijatkhanaliev <nicatxanaliev@gmail.com>
2025-10-10 03:20:59 +0400

    Events created

---
+++ ms-payment/src/main/java/com/company/model/events/OrderCreatedEvent.java
@@ -0,0 +1,15 @@
+package com.company.model.events;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class OrderCreatedEvent {
+    private String eventId;
+    private Long orderId;
+    private Long userId;
+    private BigDecimal totalPrice;
+}

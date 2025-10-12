b06c197217aa4eba10ec31415b4ee27afec969dd
nijatkhanaliev <nicatxanaliev@gmail.com>
2025-10-12 23:11:14 +0400

    OrderStatus changed

--- ms-order/src/main/java/com/company/model/enums/OrderStatus.java
+++ ms-order/src/main/java/com/company/model/enums/OrderStatus.java
@@ -3,9 +3,7 @@ package com.company.model.enums;
 public enum OrderStatus {
     CREATED,
     CONFIRMED,
-    PAYMENT_PENDING,
     PAYMENT_FAILED,
-    STOCK_PENDING,
     STOCK_FAILED,
     SHIPPED,
     DELIVERED,

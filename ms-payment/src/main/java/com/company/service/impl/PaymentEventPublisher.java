93e3eb4cb93d0ec4ad5735f5ccb6febb9130ed1f
nijatkhanaliev <nicatxanaliev@gmail.com>
2025-10-12 23:11:34 +0400

    PaymentEventPublisher class created

--- ms-payment/src/main/java/com/company/service/impl/PaymentEventPublisher.java
+++ ms-payment/src/main/java/com/company/service/impl/PaymentEventPublisher.java
@@ -9,9 +9,9 @@ import com.company.messaging.PaymentFailedProducer;
 import com.company.messaging.PaymentSuccessProducer;
 import com.company.model.dto.AccountResponseDTO;
 import com.company.model.dto.request.DecreaseAccountRequest;
-import com.company.model.events.StockUpdatedEvent;
 import com.company.model.events.PaymentFailedEvent;
 import com.company.model.events.PaymentSuccessEvent;
+import com.company.model.events.StockUpdatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -38,27 +38,27 @@ public class PaymentEventPublisher {
     private final PaymentSuccessProducer paymentSuccessProducer;
     private final PaymentFailedProducer paymentFailedProducer;
 
-
     @Transactional
     public void handlePayment(StockUpdatedEvent event) {
+
         Long userId = event.getUserId();
         BigDecimal totalPrice = event.getTotalPrice();
         log.info("Handle Order Created Payment. Getting user account. userId {}", userId);
         AccountResponseDTO accountResponse = accountClient.getAccountByUserId(userId);
-
+        BigDecimal userBalance = accountResponse.getBalance();
         if ("BLOCKED".equals(accountResponse.getStatus())) {
             throw new AccountBlockedException(ACCOUNT_BLOCKED_MESSAGE, ACCOUNT_BLOCKED);
         }
 
-        if (totalPrice.compareTo(accountResponse.getBalance()) > 0) {
+        if (totalPrice.compareTo(userBalance) > 0) {
             throw new InsufficientBalanceException(INSUFFICIENT_BALANCE_MESSAGE, INSUFFICIENT_BALANCE);
         }
+
         DecreaseAccountRequest decreaseAccountRequest = DecreaseAccountRequest.builder()
-                .amount(totalPrice)
+                .balance(userBalance.subtract(totalPrice))
                 .build();
 
-        log.info("Decreasing user balance, userId {}", userId);
-        accountClient.decreaseAccount(userId, decreaseAccountRequest);
+        accountClient.updateUserBalance(userId, decreaseAccountRequest);
 
         Payment payment = new Payment();
         payment.setOrderId(event.getOrderId());
@@ -77,12 +77,13 @@ public class PaymentEventPublisher {
     }
 
 
-    public void handlePaymentFailed(Long orderId, String message) {
-        log.info("handleOrderCreatedPaymentFailed method called. orderId {}", orderId);
+    public void handlePaymentFailed(StockUpdatedEvent event, String message) {
+        log.info("handleOrderCreatedPaymentFailed method called. orderId {}", event.getOrderId());
         PaymentFailedEvent failedPaymentEvent = PaymentFailedEvent.builder()
-                .orderId(orderId)
-                .status("FAILED")
+                .orderId(event.getOrderId())
+                .userId(event.getUserId())
                 .reason(message)
+                .orderItemDtos(event.getOrderItemDtos())
                 .build();
 
         paymentFailedProducer.send(ORDER_CREATED_EXCHANGE, PAYMENT_FAILED_ROUTING_KEY, failedPaymentEvent);
